9f3d80461aa299eecda335b3577fd25bc539202f
stay2003 <3107479638@qq.com>
2024-05-10 22:53:27 +0800

    环境搭建及测试

---
+++ src/test/java/com/yupi/usercenter/UsercenterApplicationTests.java
@@ -0,0 +1,22 @@
+package com.yupi.usercenter;
+
+import com.yupi.usercenter.mapper.UserMapper;
+import com.yupi.usercenter.model.User;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+@SpringBootTest
+class UsercenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // 测试环境
+        System.out.println("测试环境");
+    }
+
+
+}
